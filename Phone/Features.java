0b53059ae1b7fb54162e186b22b592dc7c0ba38b
ZehraYalcinkaya <zehrayalcinkaya2005@gmail.com>
2026-02-08 20:47:39 +0300

    editted to be more professional way

--- Phone/Features.java
+++ Phone/Features.java
@@ -1,10 +1,10 @@
 class Features {
     
-    public void sentence(String brandName){
+    public void sentence(String brandName){        // method(function)
         System.out.println(brandName+" will provide more space!");
     }
 
-    public void space(int gb){
+    public void space(int gb){                     // method(function)
         System.out.println("Your memory size is "+ gb+"GB");
     }
 
